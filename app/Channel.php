1636011bc7dd76c314f9c6b56bf6808ab8f4b4a0
Tom Morison <thomasmorison12gmail.com>
2019-11-26 11:07:45 -0800

    trying again

--- app/Channel.php
+++ app/Channel.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Channel extends Model
 {
-	protected $guarded = [];
+	protected $fillable = ['name', 'slug'];
     public function getRouteKeyName() {
         return 'slug';
 
