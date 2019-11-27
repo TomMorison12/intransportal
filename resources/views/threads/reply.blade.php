f9e4253e630a87c4dce27d0bf06de14346b2c2e3
Tom Morison <thomasmorison12gmail.com>
2019-11-27 12:22:50 -0800

    update

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,16 +1,16 @@
 <div class="card mt-2">
 
     <div class="card-header">
-        <div class="card-level">
+        <div class="level">
         <a class="flex" href="#">{{ $reply->owner->name }}</a> said {{ $reply->created_at->diffForHumans()}}</div>
-</div>
 <div>
     <form action="">
         <button type="submit" class="btn btn-default">Favorite</button>
     </form>
 </div>
     <div class="card-body">
-{{!! nl2br(e($reply->body)) !!}}
+{!! nl2br(e($reply->body)) !!}
 
     </div>
+</div>
 </div>
