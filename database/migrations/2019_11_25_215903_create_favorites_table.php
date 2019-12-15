5e1441524e6ee3c073a35f8ac4bceb5dec216904
Thomas Morison <administrator@intransportal.com>
2019-12-15 10:18:14 -0800

    updates

--- database/migrations/2019_11_25_215903_create_favorites_table.php
+++ database/migrations/2019_11_25_215903_create_favorites_table.php
@@ -20,12 +20,8 @@ class CreateFavoritesTable extends Migration
             $table->unsignedInteger('favorited_id');
             $table->string('favorited_type', 50);
             $table->timestamps();
-
-<<<<<<< HEAD
             $table->unique(['user_id', 'favorited_id', 'favorited_type']);
-=======
             $table->unique(['user_id', 'id', 'favorited_type']);
->>>>>>> 5f6b59353a837a9171f3120725b056c53d75fefe
         });
     }
 
