906f7aa9c2c1b71d011fa62791226412a23a382e
Thomas Morison <administrator@intransportal.com>
2020-03-17 06:15:11 -0700

    sdf

--- config/database.php
+++ config/database.php
@@ -56,7 +56,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'intransportal'),
             'username' => env('DB_USERNAME', 'intransportal'),
-            'password' => env('DB_PASSWORD', 'MetrosAreAwesome'),
+            'password' => env('DB_PASSWORD', 'Metrosawesome'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
